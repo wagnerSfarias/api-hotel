1eddb8c036f2cbabddde4d4d9f6c31f26d8dabb5
wagnerSfarias <wagner@universalhost.com.br>
2023-07-18 22:28:05 -0300

    first commit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,42 @@
+import { v4 } from 'uuid'
+import * as Yup from 'yup'
+import User from '../models/User'
+
+class UserController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().required().min(6),
+      admin: Yup.boolean(),
+    })
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { name, email, password_hash, admin } = request.body
+
+    const emailExists = await User.findOne({
+      where: { email },
+    })
+
+    if (emailExists) {
+      return response.status(409).json({ error: 'User already exist' })
+    }
+
+    const user = await User.create({
+      id: v4(),
+      name,
+      email,
+      password_hash,
+      admin,
+    })
+
+    return response.status(201).json({ id: user.id, name, email, admin })
+  }
+}
+
+export default new UserController()
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+import UserController from './app/controllers/UserController'
+
+const routes = Router()
+
+routes.post('/user', UserController.store)
+
+export default routes
