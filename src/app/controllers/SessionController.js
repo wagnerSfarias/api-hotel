2ab86c73af805ad063285aa27a88192deb2f6c3b
wagnerSfarias <wagner@universalhost.com.br>
2023-07-26 12:13:33 -0300

    add token in return json

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
 import * as Yup from 'yup'
+import jwt from 'jsonwebtoken'
 import User from '../models/User'
+import authConfig from '../../config/auth'
 
 class SessionController {
   async store(request, response) {
@@ -37,6 +39,9 @@ class SessionController {
       email: user.email,
       name: user.name,
       admin: user.admin,
+      token: jwt.sign({ id: user.id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
     })
   }
 }
