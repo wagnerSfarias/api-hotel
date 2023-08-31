973d8b73f7d7ab236cfa97f5981a2ca04ff48d6d
wagnerSfarias <wagner@universalhost.com.br>
2023-08-31 12:03:52 -0300

    first commit controller ListReservation by user

---
+++ src/app/controllers/ListReservationByUserController.js
@@ -0,0 +1,35 @@
+import Reservation from '../models/Reservation'
+import User from '../models/User'
+import Bedroom from '../models/Bedroom'
+import Unit from '../models/Unit'
+
+class ListReservationByUserController {
+  async index(request, response) {
+    const reservation = await Reservation.findAll({
+      where: {
+        user_id: request.userId,
+      },
+      attributes: ['id', 'check_in', 'check_out'],
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+        {
+          model: Bedroom,
+          as: 'bedroom',
+          attributes: ['name', 'url_banner'],
+          include: {
+            model: Unit,
+            as: 'unidade',
+            attributes: ['name', 'address'],
+          },
+        },
+      ],
+    })
+
+    return response.json(reservation)
+  }
+}
+export default new ListReservationByUserController()
