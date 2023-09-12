96dd42a1e1d9efe75cb4f158d6f4fd5f9dd75a35
wagnerSfarias <wagner@universalhost.com.br>
2023-09-12 19:57:01 -0300

    inserting atributes url image

--- src/app/controllers/ListReservationByUserController.js
+++ src/app/controllers/ListReservationByUserController.js
@@ -19,7 +19,7 @@ class ListReservationByUserController {
         {
           model: Bedroom,
           as: 'bedroom',
-          attributes: ['name', 'url_banner'],
+          attributes: ['name', 'url_banner', 'url'],
           include: {
             model: Unit,
             as: 'unidade',
