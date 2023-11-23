f67574ea405918f26b00f25859021ea03e03077d
wagnerSfarias <wagner@universalhost.com.br>
2023-11-22 22:48:39 -0300

    changing column name

--- src/database/migrations/20230720200432-crete-reservations.js
+++ src/database/migrations/20230720200432-crete-reservations.js
@@ -18,11 +18,11 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      createdAt: {
+      created_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      updatedAt: {
+      updated_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
